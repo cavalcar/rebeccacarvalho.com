eaaa627bde44084c8fbb7fff6b72a0ffc77be070
michaelb <michaelpb@gmail.com>
2024-01-16 08:16:19 -0800

    more work

--- src/static/components/Page/Page.js
+++ src/static/components/Page/Page.js
@@ -1,10 +1,24 @@
 !DOCTYPE_MODULO(`
 
 // Add custom, one-off JavaScript here for Page files
+function getLang() {
+    if (navigator.languages != undefined) {
+        for (const lang of navigator.languages) {
+            if (lang.toLowerCase().includes('pt')) {
+                return 'pt';
+            }
+        }
+    }
+    return 'en';
+}
+
 
 function prepareCallback() {
     // Get the current year on every render of the page
+    const params = new window.URLSearchParams(window.location.search)
+    const lang = params.get('lang') || getLang();
     return {
+        lang,
         currentYear: (new Date()).getFullYear(),
     };
 }
