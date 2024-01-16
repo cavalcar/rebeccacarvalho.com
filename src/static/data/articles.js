a1ed0718e6fa7da042e0771f0fd9b010be8f0027
michaelb <michaelpb@gmail.com>
2024-01-16 12:18:10 -0800

    translations

---
+++ src/static/data/articles.js
@@ -0,0 +1,40 @@
+!DOCTYPE_MODULO(JSON.stringify(
+{
+
+    en2pt: {
+        article_example1: 'artigo_exemplo1',
+        article_example2: 'artigo_exemplo2',
+    },
+
+    en: [
+        {
+            title: "Example 1 article",
+            byline: "Rebecca Carvalho",
+            estimate: "5 minute read",
+            file: "article_example1.html",
+        },
+        {
+            title: "Example 2 article",
+            byline: "Rebecca Carvalho",
+            estimate: "10 minute read",
+            file: "article_example2.html"
+        }
+    ],
+
+    pt: [
+        {
+            title: "Examplo 1 artigo",
+            byline: "Rebecca Carvalho",
+            estimate: "5 minutos",
+            file: "artigo_examplo1.html"
+        },
+        {
+            title: "Examplo 2 artigo",
+            byline: "Rebecca Carvalho",
+            estimate: "10 minutos",
+            file: "artigo_examplo2.html"
+        }
+    ],
+
+}
+))
---
+++ src/static/data/pages.js
@@ -0,0 +1,53 @@
+!DOCTYPE_MODULO(JSON.stringify(
+{
+
+    en2pt: {
+        index: 'index',
+        about: 'biografia',
+        writing: 'artigos',
+        contact: 'contato',
+    },
+
+    en: [
+        {
+            title: "Rebecca Carvalho",
+            file: "index.html",
+            style: "home",
+        },
+        {
+            title: "Writing",
+            file: "writing.html"
+        },
+        {
+            title: "About",
+            file: "about.html"
+        },
+        {
+            title: "Contact",
+            file: "contact.html"
+        },
+    ],
+
+    pt: [
+        {
+            title: "Rebecca Carvalho",
+            file: "index.html",
+            style: "home"
+        },
+        {
+            title: "Artigos",
+            file: "artigos.html"
+        },
+        {
+            title: "Biografia",
+            file: "biografia.html"
+        },
+        {
+            title: "Contato",
+            file: "contato.html"
+        },
+    ],
+
+
+}
+))
